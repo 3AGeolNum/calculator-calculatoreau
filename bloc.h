3a9594cfcee86fa6c0cb029528dbdab6f7f65be1
Quentin Capillon <capillon1u@etu.univ-lorraine.fr>
2020-09-23 16:27:40 +0200

    sh: q: command not found acceseur et initialisateur

--- bloc.h
+++ bloc.h
@@ -3,13 +3,18 @@
 
 template<class T> class Bloc{
 	public:
-		Bloc( int nature, T item, Bloc* ptr_bas1 = nullptr, Bloc* ptr_bas2 = nullptr, Bloc* ptr_haut = nullptr );
+		Bloc( int nature, T item, Bloc* ptr_haut = nullptr , Bloc* ptr_bas1 = nullptr, Bloc* ptr_bas2 = nullptr);
 		~Bloc();
 		void Set_nature(int nature); //0 for nomber and 1 for operator
 		int Get_nature();
 		void Set_valeur();
 		T Get_valeur();
-		void Set_ptr();
+		void Set_ptr_haut(Bloc* ptr);
+		void Set_ptr_bas1(Bloc* ptr);
+		void Set_ptr_bas1(Bloc* ptr);
+		Bloc* get_ptr_haut();
+		Bloc* get_ptr_bas1();
+		Bloc* get_ptr_bas2();
 	private :
 		int nature_;//0 for nomber and 1 for operator
 		T item_;
@@ -17,6 +22,6 @@ template<class T> class Bloc{
 		Bloc* ptr_bas1_;
 		Bloc* ptr_bas2_;
 		
-}
+};
 
 #endif
