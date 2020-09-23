87d9a0e2a205438cc929c3970317fe277d43bf07
Julien Herrero <herrero4u@etu.univ-lorraine.fr>
2020-09-23 16:53:48 +0200

    fonction evaluator prototype

---
+++ evaluator.cpp
@@ -0,0 +1,26 @@
+#include "evaluator.h"
+#include <iostream>
+
+void Bloc::evaluator(Bloc const& a, Bloc const& b)
+{
+	if (a*b)
+	{
+
+
+	}
+	else if (a+b)
+	{
+
+
+	}
+	else if (a-b)
+	{
+
+
+	}
+	else if (a/b)
+	{
+
+
+	}
+}
