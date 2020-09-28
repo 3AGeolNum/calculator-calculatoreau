7ec5843dba7e14274f0bc3f30ef0a36d577d6e9e
Julien Herrero <herrero4u@etu.univ-lorraine.fr>
2020-09-28 16:49:56 +0200

    evaluator v1.01

--- evaluator.cpp
+++ evaluator.cpp
@@ -1,26 +1,33 @@
 #include "evaluator.h"
 #include <iostream>
 
-void Bloc<T>::evaluator(Bloc* const& a, Bloc* const& b)
+void Bloc::evaluator (DynArray<Bloc*> const& a, DynArray<Bloc*> const& op DynArray<Bloc*> const& b)
 {
-	if (a*b)
+	double calc;
+	for (int ia(0); ia < calc.size(); ++ia )
 	{
 
+	if (op ="*")
+	{
+	calc = a * b;
+	}
+	else if (op = "+")
+	{
+
+		calc = a + b;
 
 	}
-	else if (a+b)
+	else if (op = "-")
 	{
 
+		calc = a - b;
 
 	}
-	else if (a-b)
+	else if (op = "/")
 	{
 
+		calc = a / b;
 
 	}
-	else if (a/b)
-	{
-
-
 	}
 }
---
+++ evaluator.h
@@ -0,0 +1,1 @@
+void evaluator(DynArray<Bloc*> const& a, DynArray<Bloc*> const& op DynArray<Bloc*> const& b);
