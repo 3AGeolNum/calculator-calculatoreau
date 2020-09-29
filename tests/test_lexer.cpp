4e7abf9b98b7aa53870b411677712d7410ca00d2
Quentin Capillon <capillon1u@etu.univ-lorraine.fr>
2020-09-29 16:18:54 +0200

    test_lexer.cpp update tests

---
+++ tests/test_lexer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <CalculetteProject.h>
+#include <calc_lib/lexer.h>
+#include <calc_lib/bloc.h>
+#include <calc_lib/DynArray.h>
+
+int main(){
+	//création d'une chaine de calcul.
+	std::string chaine_test = "-45-9+89*-4";
+	//Création d'un DynArray de bloc contenant les parties élémentaires de la chaine.
+	DynArray<Bloc*> retour (lexer(chaine_test));
+	//Affichage du DynArray.
+	for (int ia = 0 ; ia < retour.size(); ++ia){
+		std::cout << "Element de rang " << ia << " est " << retour[ia]->get_valeur() << std::endl;
+	}
+}
