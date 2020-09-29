3defa7a1130141145b7ddd46973bed65cac39169
YAYA <jpiau27etu.univ-lorraine.fr>
2020-09-29 16:50:35 +0200

    Correction du test et retrait du build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: O:/Documents/ENSG_3A/Projet_calculette/Clonage
-# Build directory: O:/Documents/ENSG_3A/Projet_calculette/Clonage/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/calc_lib")
-subdirs("mains")
-subdirs("tests")
