d6af93200c45090eb234595ca4bb49359f609893
YAYA <jpiau27etu.univ-lorraine.fr>
2020-09-29 08:35:22 +0200

    ajout d'un test du lexer

--- mains/main.cpp
+++ mains/main.cpp
@@ -6,10 +6,6 @@
 #include <calc_lib/DynArray.h>
 
 int main(){
-	//main yas pour tester mes modifs
-	std::string chaine_test = "-45-9+89*-4";
-	DynArray<Bloc*> retour (lexer(chaine_test));
-	for (int ia = 0 ; ia < retour.size(); ++ia){
-		std::cout << "Element de rang " << ia << " est " << retour[ia]->get_valeur() << std::endl;
-	}
+	std::cout << "nous sommes dans le main " << std::endl;
+	
 }
