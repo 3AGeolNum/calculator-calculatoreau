8f8a6b2ec8bff1849d521ef13c93a155c76a6d07
Quentin Capillon <capillon1u@etu.univ-lorraine.fr>
2020-10-02 10:29:40 +0200

    CMakeLists.txt ur les conflicts

--- include/calc_lib/lexer.h
+++ include/calc_lib/lexer.h
@@ -3,4 +3,6 @@
 #include <calc_lib/bloc.h>
 #include <calc_lib/DynArray.h>
 
+void copy_local(DynArray<Bloc*>& new_tab, DynArray<Bloc*>& local_tab_copied);
+DynArray<Bloc*> cleaner_tab(const DynArray<Bloc*>& other, int size);
 DynArray<Bloc*> lexer(const std::string& input);
