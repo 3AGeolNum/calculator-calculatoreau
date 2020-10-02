2a07190f05a541aa1687842a26493454596f25bc
Quentin Capillon <capillon1u@etu.univ-lorraine.fr>
2020-10-02 11:02:12 +0200

    kk

---
+++ mains/main.cpp
@@ -0,0 +1,12 @@
+  
+#include <iostream>
+#include <string>
+#include <CalculetteProjectConfig.h>
+#include <calc_lib/lexer.h>
+#include <calc_lib/bloc.h>
+#include <calc_lib/DynArray.h>
+
+int main(){
+	std::cout << "nous sommes dans le main " << std::endl;
+	
+}
