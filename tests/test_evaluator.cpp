6a01f2c0913826054293776244c3f0c3b6ef69fa
YassinePE <71491786+YassinePE@users.noreply.github.com>
2020-10-05 15:51:04 +0200

    Update test_evaluator.cpp

--- tests/test_evaluator.cpp
+++ tests/test_evaluator.cpp
@@ -15,7 +15,7 @@ int main() {
 	std::string calcul = "-45-9+89*-4";
 	DynArray<Bloc*> calcul_bloc = lexer(calcul);
 	DynArray<Bloc*> parsed_calcul = parser(calcul_bloc);
-	std::cout << "Le resultat est : " << evaluator(parsed_calcul) << std::endl;
+	//std::cout << "Le resultat est : " << evaluator(parsed_calcul) << std::endl;
 	//}while(calcul != "0");
 	
 	if(atof((evaluator(parsed_calcul)).c_str()) != -410.0){
